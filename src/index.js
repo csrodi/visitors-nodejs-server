bf502fd65139a106e5126fd10d54b9df9542b184
Carlos <rodriguez@ebu.ch>
2019-01-22 14:21:16 +0100

    Adding basic authentication to all routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ const fastify = require('fastify')({
   logger: true
 })
 
+// Authentication stuff
+const authenticate = {realm: 'Visitors'}
+fastify.register(require('fastify-auth'))
+fastify.register(require('fastify-basic-auth'), { validate, authenticate })
+async function validate (username, password, req, reply) {
+  if (username !== 'visitor' || password !== 'visitor') {
+    return new Error('Bad credentials!')
+  }
+}
+
 // Require external modules
 const mongoose = require('mongoose')
 
@@ -19,6 +29,9 @@ fastify.register(require('fastify-swagger'), swagger.options)
 mongoose.connect('mongodb://192.168.99.100/mycargarage')
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err))
+
+// Use preHandler to authenticate all the routes
+fastify.after(() => fastify.addHook('preHandler', fastify.auth([fastify.basicAuth])))
 
 // Loop over each route
 routes.forEach((route, index) => {
